7bbe7d9a540755989eb87915ac4d457e8a3f2c13
Jamchuck09 <98853810+Jamchuck09@users.noreply.github.com>
2022-06-09 08:52:39 -0400

    Add files via upload

---
+++ LmaoCs/LmaoCs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LmaoCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cool Stuff in the link below");
+            System.Console.Read();
+            Console.WriteLine("jamchuck09.github.io");
+            System.Console.Read();
+        }
+    }
+}
